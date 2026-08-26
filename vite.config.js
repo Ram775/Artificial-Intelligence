798b36c7d43148b5cbb88c1260a803bb91798919
Shreeram Singraul <aashikrajhpoot@gmail.com>
2026-08-26 11:53:04 +0530

    Enhance PWA settings in vite.config.js
    Updated PWA configuration with new icons and manifest details.

--- vite.config.js
+++ vite.config.js
@@ -6,35 +6,74 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),
-     VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+    VitePWA({
+      registerType: "autoUpdate",
+
+      includeAssets: [
+        "favicon.ico",
+        "apple-touch-icon.png",
+        "pwa-192.png",
+        "pwa-512.png",
+      ],
+
       manifest: {
-        name: 'Shree Ai',
-        short_name: 'ShreeAi',
-        description: 'ShreeAi',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        display: 'standalone',
+        name: "Shree Ai",
+        short_name: "Shree Ai",
+        description: "A modern artificial inteligence",
+        theme_color: "#ff4da6",
+        background_color: "#000000",
+        display: "standalone",
+        orientation: "portrait",
+        start_url: "/",
+
         icons: [
           {
-            src: 'icons/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
+            src: "/pwa-192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: 'icons/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
+            src: "/pwa-512.png",
+            sizes: "512x512",
+            type: "image/png",
           },
           {
-            src: 'icons/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
-          }
-        ]
-      }
-    })
+            src: "/pwa-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+
+      workbox: {
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+        skipWaiting: true,
+
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.destination === "image",
+            handler: "CacheFirst",
+            options: {
+              cacheName: "images-cache",
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+              },
+            },
+          },
+          {
+            urlPattern: ({ request }) =>
+              request.destination === "script" ||
+              request.destination === "style",
+            handler: "StaleWhileRevalidate",
+            options: {
+              cacheName: "assets-cache",
+            },
+          },
+        ],
+      },
+    }),
   ],
 })
